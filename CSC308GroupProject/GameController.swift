a3012c2f906a1219a3675e72c4aae4d785f0b4d8
m1n3sweeper12 <159362951+m1n3sweeper12@users.noreply.github.com>
2026-04-16 19:59:00 -0400

    added game mode selection and game screen

---
+++ CSC308GroupProject/GameController.swift
@@ -0,0 +1,19 @@
+//
+//  GameController.swift
+//  CSC308GroupProject
+//
+//  Created by Morgan, Greyson F. on 4/16/26.
+//
+
+import UIKit
+
+class GameController: UIViewController {
+    
+    var mode: Any?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ CSC308GroupProject/PlayModeController.swift
@@ -0,0 +1,60 @@
+//
+//  PlayModeController.swift
+//  CSC308GroupProject
+//
+//  Created by Morgan, Greyson F. on 4/16/26.
+//
+
+import UIKit
+
+class PlayModeController: UIViewController {
+    
+    var mode = Mode.SP
+    
+    @IBAction func playButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "playSegue", sender: self)
+    }
+    @IBOutlet weak var modeDescText: UITextView!
+    
+    
+    @IBAction func modeChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: // single player selected
+            modeDescText.text = "Single Player\n\nIn single player mode, you play against Al the AI, trying to create a row, column, or diagonal of four chips before it can."
+            mode = Mode.SP
+            break
+        case 1: // two player selected
+            modeDescText.text = "Two Player\n\nIn two player mode, you and a friend can play against each other, taking turns trying to make a row, column, or diagonal of your chips before they can."
+            mode = Mode.TP
+            break
+        case 2: // two player timed selected
+            modeDescText.text = "Two Player Timed\n\nIn two player timed mode, you and a friend can play against each other, trying to make a row, column, or diagonal of your chips before they can. But be careful! If you don't place your chip in time, your turn is skipped and play moves to the next player!"
+            mode = Mode.TPT
+            break
+        default:
+            break
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        modeDescText.text = "Single Player\n\nIn single player mode, you play against Al the AI, trying to create a row, column, or diagonal of four chips before it can."
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "playSegue" {
+            guard let dest = segue.destination as? GameController else {
+                return
+            }
+            dest.mode = mode
+        }
+    }
+
+}
+
+enum Mode {
+    case SP
+    case TP
+    case TPT
+}
